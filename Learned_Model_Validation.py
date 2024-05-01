bbbcf8448f67cfa5e405270ea1a2edcb7377cadb
shoubhaniknath <159340373+shoubhaniknath@users.noreply.github.com>
2024-04-30 19:50:35 -0700

    Update Learned_Model_Validation.py

--- Learned_Model_Validation.py
+++ Learned_Model_Validation.py
@@ -4,6 +4,13 @@
 Created on Thu Mar 21 17:09:52 2024
 
 @author: shoubhaniknath
+
+        Python script to validate learned model for Si ALE with Cl/Ar for any condition of Ar ion fluence and energy
+
+        There are some redundancies in this script, e.g., not all the variables are being sent to device, and hence 
+        this can be used only on CPU (which is fine for model validation)
+
+        Currently, this script can only be used for one condition at a time. 
 """
 
 # Load the model
@@ -15,72 +22,11 @@ import matplotlib.pyplot as plt
 from torchdiffeq import odeint
 import sys
 sys.path.append("/Users/shoubhaniknath/my_modules/")
-#import ale_learning as ALE
-import ale_learning_2 as ALE
+import ale_learning as ALE
 import random
 
-## Custom activation function
-# class WeightedSigmoid(nn.Module):
-#     def __init__(self, in_feature, output):
-#         super(WeightedSigmoid, self).__init__()
-        
-#         self.weight = torch.abs(nn.Parameter(torch.randn(in_feature, output, dtype=torch.float32))).clone().detach().requires_grad_(True)
-        
-#     def forward(self, inp):
-#         activation = self.weight * 1 / (1 + torch.exp(-inp))
-#         return activation
-
-# class NeuralNet(nn.Module):
-#     def __init__(self):
-#         super(NeuralNet, self).__init__()
-        
-#         self.activation = WeightedSigmoid(in_feature=1, output=1)
-#         self.transition_rate = nn.Sequential(
-#                     nn.Linear(3,256),
-#                     nn.Tanh(),
-#                     nn.Linear(256,1),
-#                     self.activation
-#             )    
-#     def forward(self, x):
-#         return self.transition_rate(x)
-
-# class NeuralNet(nn.Module):
-#     def __init__(self):
-#         super(NeuralNet, self).__init__()
-        
-#         self.activation = WeightedSigmoid(in_feature=1, output=1)
-#         self.transition_rate = nn.Sequential(
-#                     nn.Linear(3,128),
-#                     nn.Tanh(),
-#                     nn.Linear(128,128),
-#                     nn.Tanh(),
-#                     nn.Linear(128,1),
-#                     self.activation
-#             )
-        
-#     def forward(self, x):
-#         return self.transition_rate(x)
-
-# class NeuralNet(nn.Module):
-#     def __init__(self):
-#         super(NeuralNet, self).__init__()
-        
-#         self.activation = WeightedSigmoid(in_feature=1, output=1)
-#         self.transition_rate = nn.Sequential(
-#                     nn.Linear(3,128),
-#                     nn.Tanh(),
-#                     nn.Linear(128,128),
-#                     nn.Tanh(),
-#                     nn.Linear(128,1),
-#                     self.activation
-#             )
-        
-#     def forward(self, x):
-#         return self.transition_rate(x)
-
+## Define the neural network model
 # Custom activation function
-# USE THIS FORMULATION!!!
-
 class WeightedSigmoid(nn.Module):
     def __init__(self, in_feature):
         super(WeightedSigmoid, self).__init__()
@@ -121,11 +67,11 @@ class NeuralNet(nn.Module):
     
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-kc1 = NeuralNet()
+kc1 = NeuralNet().to(device)
 
-kc3 = NeuralNet()
+kc3 = NeuralNet().to(device)
 
-ks3 = NeuralNet()
+ks3 = NeuralNet().to(device)
 
 kc1.load_state_dict(torch.load('Learned_Model_Including_Fluence_v_1e_11_D_1e_19.pth')['net1_state_dict'])
 kc3.load_state_dict(torch.load('Learned_Model_Including_Fluence_v_1e_11_D_1e_19.pth')['net2_state_dict'])
@@ -150,7 +96,6 @@ c0 = torch.zeros((len(impacts), len(energies), len(xvec)), dtype=torch.float32) 
 
 #t, x_test, init_test, energy, fluence = ale_learning.DataPreSiALE(directory, impacts, energies, c0)
 
-
 # Define some system parameters
 tsim = 30.0             # Time of experiment in s. This time is taken from Chen et al, Jpn. J. Appl. Phys., Vol. 44, No. 1A (2005)
 D = torch.tensor(1e-19, dtype=torch.float32)              # Units of m^2/s: Ho, Surf Sci (1978) 253-263
@@ -158,6 +103,7 @@ A = 1063e-20            # Area of surface (from simulation) in m^2
 n_Si = torch.tensor(5e28, dtype=torch.float32)             # Density of Si in number/m^3
 n_Surface = torch.tensor(72/1063e-20, dtype=torch.float32) # 72 atoms in plane Vella et al, J Vac Sci Technol (2022)
 Nc = torch.tensor(A * 5.8e18, dtype=torch.float32)         # Simulation area = 1063 A^2: Vella et al, J Vac Sci Technol (2022). Active site conc 5.8e18 molecules/m^2: Gao, J Chem Phys (1993)
+v_guess = 1e-11
 
 # Package system parameters to pass to the ODE solver
 constants = [tsim, D, A, n_Si, n_Surface, Nc, delX]
@@ -165,7 +111,7 @@ constants = [tsim, D, A, n_Si, n_Surface, Nc, delX]
 # List of neural network models
 rate_list = [kc1, kc3, ks3] 
 
-time, y_actual, init, energy_tensor, fluence_val = ALE.DataPreSiALE(directory, impacts, energies, c0, Diffusion=D, v_guess=1e-11)
+time, y_actual, init, energy_tensor, fluence_val = ALE.DataPreSiALE(directory, impacts, energies, c0, Diffusion=D, v_guess=v_guess)
 ODEFunc = ALE.SiALE_ODEfunc
 
 #params = [fluence_val, energy_tensor, constants, rate_list] # only energy
@@ -179,5 +125,6 @@ print(f'Loss on test data = {val_loss.item():.5e}\n')
 
 ALE.StateProbPlot(t, y_pred[...,-3:], y_actual, energies, impacts, ['II','III','I'])
 
+# Some issues with this method
 #ALE.GoodnessOfFit(y_actual.squeeze(1), y_pred[:,:,-3:].squeeze(1))
 
